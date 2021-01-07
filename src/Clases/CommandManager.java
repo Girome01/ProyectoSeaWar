afae4482b89e73dc81d9d88f9eeee867a2e0ce92
Girome <gilrome2002@estudiantec.cr>
2021-01-06 19:13:22 -0600

    Creacion de los command base

---
+++ src/Clases/CommandManager.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Gilberth
+ */
+public class CommandManager {
+    private static CommandManager commandManager;
+    private static final HashMap<String, Class<? extends ICommand>> COMMANDS =          
+            new HashMap<String, Class<? extends ICommand>>();  
+    
+    private CommandManager() {           
+           
+    } 
+    
+    public static synchronized CommandManager getIntance() {           
+        if (commandManager == null) {               
+            commandManager = new CommandManager();   
+        }
+        return commandManager;   
+    }
+    
+    public ICommand getCommand(String commandName) {           
+        if (COMMANDS.containsKey(commandName.toUpperCase())) {               
+            try {                   
+                return COMMANDS.get(commandName.toUpperCase()).newInstance();
+            } catch (Exception e) {   e.printStackTrace();                   
+            //return new ErrorCommand();
+            return null;
+            }           
+        } 
+        else {
+            //return new NotFoundCommand();   
+            return null;
+        }  
+    }
+    
+    public void registCommand(String commandName, Class<? extends ICommand> command) {   
+        COMMANDS.put(commandName.toUpperCase(), command);   
+    } 
+    
+}
